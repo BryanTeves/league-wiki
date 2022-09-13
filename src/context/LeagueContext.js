a9d51c99d9601f6caffdffaa46040e3d33da3ba4
BryanTeves <bryanteves2013@hotmail.com>
2022-09-13 16:15:42 -0300

    new security context

--- src/context/LeagueContext.js
+++ src/context/LeagueContext.js
@@ -6,7 +6,7 @@ export const LeagueProvider = function ({ children }) {
   // Get the champion data
   const championName = async (name) => {
     const getData = await fetch(
-      `http://ddragon.leagueoflegends.com/cdn/12.16.1/data/en_US/champion/${name}.json`
+      `https://ddragon.leagueoflegends.com/cdn/12.16.1/data/en_US/champion/${name}.json`
     );
     const dataFetched = await getData.json();
 
@@ -17,21 +17,21 @@ export const LeagueProvider = function ({ children }) {
 
   // Get the champion icon image
   const championImage = (name) => {
-    const image = `http://ddragon.leagueoflegends.com/cdn/12.16.1/img/champion/${name}.png`;
+    const image = `https://ddragon.leagueoflegends.com/cdn/12.16.1/img/champion/${name}.png`;
 
     return image;
   };
 
   // Get the background champion image
   const backgroundImage = (name, num = "0") => {
-    const image = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${name}_${num}.jpg`;
+    const image = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${name}_${num}.jpg`;
 
     return image;
   };
 
   const allChampions = async (name) => {
     const getData = await fetch(
-      `http://ddragon.leagueoflegends.com/cdn/12.17.1/data/en_US/champion.json`
+      `https://ddragon.leagueoflegends.com/cdn/12.17.1/data/en_US/champion.json`
     );
 
     const dataFetched = await getData.json();
